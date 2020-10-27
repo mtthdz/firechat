ee93d0817fee11a1517e548f2dfde4246e9a2176
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-10-27 16:46:11 -0400

    added firestore and auth to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore'; // firestore db is real time
+import 'firebase/auth'; // enables google login
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+// refactored version of Firebase config
+firebase.initializeApp({
+  apiKey: "AIzaSyB8U8eh0fVLaKGTWVgoHagP9J1XGI8LIkk",
+  authDomain: "firechat-c4b22.firebaseapp.com",
+  databaseURL: "https://firechat-c4b22.firebaseio.com",
+  projectId: "firechat-c4b22",
+  storageBucket: "firechat-c4b22.appspot.com",
+  messagingSenderId: "894535656815",
+  appId: "1:894535656815:web:6908e728cf0a52d328f49a"
+})
+
+// firebase SDKs set as global variables
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
